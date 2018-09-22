a3b840a1e5f5f2b9385af8ce4d19d9f3bda0f605
Matthew Lymer <matthew@lymer.ca>
2018-09-22 09:26:16 -0400

    Add example program

---
+++ src/ConsoleApp1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    internal static class Program
+    {
+        public static void Main(string[] args)
+        {
+            if (args.Length > 0)
+            {
+                var firstArg = args[0];
+
+                if (firstArg == null || firstArg.Equals(""))
+                {
+                    // TODO: update exception
+                    throw new Exception($"This is bad code: {firstArg.Length}");
+                }
+            }
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
