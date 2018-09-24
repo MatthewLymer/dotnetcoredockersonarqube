df71472373a39f7fd30c3f4f64f1052df6dc1f9b
Matthew Lymer <matthew.lymer@tripstack.com>
2018-09-24 14:51:36 -0400

    Get the stuff actually working

--- containers/sonarqube-analyze/docker-entrypoint.sh
+++ containers/sonarqube-analyze/docker-entrypoint.sh
@@ -5,9 +5,6 @@ until [ "$(curl --silent --output /dev/null --write-out '%{http_code}' $SonarHos
     sleep 3
 done
 
-echo Found $SonarHostUrl, waiting 10 seconds
-sleep 10
-
 dotnet sonarscanner begin /k:"MySolution" /d:"sonar.host.url"=$SonarHostUrl
 dotnet build $DotnetCoreSlnPath
 dotnet sonarscanner end
